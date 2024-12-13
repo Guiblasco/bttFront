c02eb8d9ec985cf947a39cf5437c2c1ae8f5b8e4
Guillem Blasco <gowet92@gmail.com>
2024-12-13 10:36:51 +0100

    Change App component from RouterProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/App/App.tsx";
 import "@fontsource/poppins/600.css";
 import "./styles/styles.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./router/router.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
